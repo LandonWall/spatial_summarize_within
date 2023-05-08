e613d6221b3e88059191e30c86845c68617dd549
Landon Wall <kennethlandonwall@gmail.com>
2023-05-08 16:20:38 -0700

    v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spatial_summarize_within",
-    version="0.1.2",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "geopandas",
