9c89a1f5b9f58dbffbbcc37287f5713c2bc0a313
Landon Wall <kennethlandonwall@gmail.com>
2023-05-11 21:21:47 -0700

    v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spatial_summarize_within",
-    version="0.2.0",
+    version="0.2.1",
     Author = "Kenneth Landon Wall",
     Email = "Landon@AlloyAnalytics.org",
     packages=find_packages(),
